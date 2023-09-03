f3a8cb19ff99e0b1688136ffc36ccf3d72b69cac
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-09-03 20:49:29 +0700

    Insert Document

--- mongodb.js
+++ mongodb.js
@@ -7,3 +7,38 @@ db.createCollection("orders");
 db.getCollectionNames();
 
 db.customers.find()
+
+db.customers.insertOne({
+    _id: "khannedy",
+    name: "Eko Kurniawan Khannedy"
+})
+
+db.products.insertMany([
+    {
+        _id : 1,
+        name: "Indomie Ayam Bawang",
+        price: new NumberLong("2000")
+    },
+    {
+        _id : 2,
+        name: "Mie Sedap Soto",
+        price: new NumberLong("2000")
+    }
+])
+
+db.orders.insertOne({
+    _id: new ObjectId(),
+    total: new NumberLong("8000"),
+    items: [
+        {
+            product_id: 1,
+            price: new NumberLong("2000"),
+            quantity: new NumberInt("2")
+        },
+        {
+            product_id: 2,
+            price: new NumberLong("2000"),
+            quantity: new NumberInt("2")
+        }
+    ]
+})
