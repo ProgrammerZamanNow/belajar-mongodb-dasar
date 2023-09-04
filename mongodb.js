c315bb825fbb03509faa141eac9b1c6b3f654a00
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-09-04 12:43:04 +0700

    array operator

--- mongodb.js
+++ mongodb.js
@@ -395,3 +395,143 @@ db.products.updateMany({}, {
         }
     }
 })
+
+db.products.updateMany({}, {
+    $set: {
+        ratings: [90, 80, 70]
+    }
+})
+
+db.products.updateMany({
+    ratings: 90
+}, {
+    $set: {
+        'ratings.$': 100
+    }
+})
+
+db.products.updateMany({}, {
+    $set: {
+        'ratings.$[]': 100
+    }
+})
+
+db.products.updateMany({}, {
+    $set: {
+        ratings: [90, 80, 70]
+    }
+})
+
+db.products.updateMany({}, {
+    $set: {
+        'ratings.$[element]' : 100
+    }
+}, {
+    arrayFilters: [
+        {
+            element: {
+                $gte: 80
+            }
+        }
+    ]
+})
+
+db.products.updateMany({}, {
+    $set: {
+        'ratings.0': 50,
+        'ratings.1': 60
+    }
+})
+
+db.products.find({_id: 1})
+
+db.products.updateOne({_id: 1}, {
+    $addToSet: {
+        tags: 'popular'
+    }
+})
+
+db.products.updateOne({_id: 1}, {
+    $pop: {
+        ratings: -1
+    }
+})
+
+db.products.find({_id: 2})
+
+db.products.updateOne({_id: 2}, {
+    $pop: {
+        ratings: 1
+    }
+})
+
+db.products.updateMany({}, {
+    $set: {
+        ratings: [90, 80, 70]
+    }
+})
+
+db.products.updateMany({}, {
+    $pull: {
+        ratings: {
+            $gte: 80
+        }
+    }
+})
+
+db.products.updateMany({}, {
+    $push: {
+        ratings: 0
+    }
+})
+
+db.products.updateMany({}, {
+    $pullAll: {
+        ratings: [100, 0]
+    }
+})
+
+db.products.updateMany({}, {
+    $push: {
+        ratings: {
+            $each: [100, 200, 300]
+        }
+    }
+})
+
+
+db.products.updateMany({}, {
+    $addToSet: {
+        tags: {
+            $each: ['trending', 'popular']
+        }
+    }
+})
+
+db.products.updateMany({}, {
+    $push: {
+        tags: {
+            $each: ['hot'],
+            $position: 1
+        }
+    }
+})
+
+db.products.updateMany({}, {
+    $push: {
+        ratings: {
+            $each: [100, 200, 300, 400, 500],
+            $sort: -1
+        }
+    }
+})
+
+db.products.updateMany({}, {
+    $push: {
+        ratings: {
+            $each: [100, 200, 300, 400, 500],
+            $slice: 10,
+            $sort: -1
+        }
+    }
+})
