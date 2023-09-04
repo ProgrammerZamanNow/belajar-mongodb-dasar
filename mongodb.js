5ef766fc9ba7f5da44b3e1ff76b1937b368cee40
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-09-04 15:37:02 +0700

    bulk write

--- mongodb.js
+++ mongodb.js
@@ -565,3 +565,36 @@ db.customers.deleteMany({
         $regex: "spammer"
     }
 })
+
+db.customers.bulkWrite([
+    {
+        insertOne: {
+            document: {
+                _id: "eko",
+                full_name: "Eko"
+            }
+        }
+    },
+    {
+        insertOne: {
+            document: {
+                _id: "kurniawan",
+                full_name: "Kurniawan"
+            }
+        }
+    },
+    {
+        updateMany: {
+            filter: {
+                _id: {
+                    $in: ['eko', 'kurniawan', 'khannedy']
+                }
+            },
+            update: {
+                $set: {
+                    full_name: 'Eko Kurniawan Khannedy'
+                }
+            }
+        }
+    }
+])
